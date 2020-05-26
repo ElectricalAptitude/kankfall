a4071e7fe46caf3db735d5e2b3cd4be737a62592
ElectricalAptitude <matthew.gospel.johnstone@gmail.com>
2020-05-26 11:00:31 -0500

    Added sample API calls
    Added a few sample API calls, though I can't get them to format correctly in Python3 yet, so...

--- kankfall.py
+++ kankfall.py
@@ -1,3 +1,11 @@
 # A simply Python script to pull data regarding cards from Scryfall.com and input them as entities into kanka.io 
 # Open Source blah blah blah Creative Commons blah blah GNU License blah blah Wizards of the Coast Fan Content Policy blah
 
+import json
+
+https://api.scryfall.com/cards/named?exact="curio vendor"&
+  https://api.scryfall.com/cards/search?q=c%3Awhite+cmc%3D1
+  https://api.scryfall.com/cards/search?q=e%3DKLD+e%3DAER
+    
+   
+    
