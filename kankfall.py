99124935402da6507cab07b8cca64d4eb466a56a
ElectricalAptitude <matthew.gospel.johnstone@gmail.com>
2020-05-26 09:36:51 -0500

    Create kankfall.py
    first creation of the file, for the redirect URL required by kanka.io for creating keys. Also some fake licensing info

---
+++ kankfall.py
@@ -0,0 +1,3 @@
+# A simply Python script to pull data regarding cards from Scryfall.com and input them as entities into kanka.io 
+# Open Source blah blah blah Creative Commons blah blah GNU License blah blah Wizards of the Coast Fan Content Policy blah
+
