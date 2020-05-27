e389a6ec5795ffdf0793725ae94e31133a375b04
ElectricalAptitude <matthew.gospel.johnstone@gmail.com>
2020-05-27 16:18:49 -0600

    did I actually save after creating the file? You bet I did.

--- ids.py
+++ ids.py
@@ -1,1 +1,20 @@
-''' 
+''' we got a whole bunch of static ids up in here...why not do the thing up right?
+
+Riffing on the suggestions from https://www.programiz.com/python-programming/variables-constants-literals
+
+ATM, this module is proposed, and not in production
+'''
+
+# Locations
+KALADESH_LOC = 174953
+RAVNICA_LOC = 174971
+THEROS_LOC = 176830
+
+# Tags
+KALADESH_TAG = 54084
+RAVNICA_TAG = 54085
+THEROS_TAG = 54177
+KANKFALL_TAG = 55903
+
+# Races
+RACES = {"Aetherborn":67177, "Centaur":67037, "Construct":68699, "Dwarf":67077, "Elf":67493, "Goblin":66961, "Human":66977, "Vedalken":66954}
